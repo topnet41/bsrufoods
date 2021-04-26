20cd1c56a73159aee538beb6e371f603c0afcb21
6021207008 <6021207008@gmail.com>
2021-04-27 05:47:52 +0700

    update function report_year and create widget picker and date_time_picker

--- lib/controller/report_sale.dart
+++ lib/controller/report_sale.dart
@@ -1,79 +1,138 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ReportSale{
-  FirebaseFirestore firestore  = FirebaseFirestore.instance;
+class ReportSale {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   List data = [];
-  List data_report = [];
+  List<PopulationData> data_report = [];
   List menus = [];
   var snpapshop;
 
-  Future<List> getMenu()async{
+  Future<List> getMenu() async {
     var menu;
-    menu = await firestore.collection("menus").doc(_firebaseAuth.currentUser.uid).get();
+    menu = await firestore
+        .collection("menus")
+        .doc(_firebaseAuth.currentUser.uid)
+        .get();
     menus = menu["menudetail"];
     return menus;
   }
 
-  Future<List> getdata(String shopid)async{
-    List dataReport = [] ;
+  Future<List> getdata(String shopid) async {
+    List dataReport = [];
     QuerySnapshot datalop;
     datalop = await firestore
-      .collection("orders")
-      .orderBy("orderList", descending: false)
-      .where("shopId", isEqualTo: shopid)
-      .get();
-      datalop.docs.forEach((element)async{
-            Map dataMap = Map();
-            dataMap = element.data();
-            snpapshop = await element["detail"].get();
-            dataMap["detail"] =  snpapshop["detail"];
-            dataReport.add(dataMap);
-            data = dataReport;
-      });
-    data.sort((m1,m2)=>m1["orderList"].compareTo(m2["orderList"]));
+        .collection("orders")
+        .orderBy("orderList", descending: false)
+        .where("shopId", isEqualTo: shopid)
+        .get();
+    datalop.docs.forEach((element) async {
+      Map dataMap = Map();
+      dataMap = element.data();
+      snpapshop = await element["detail"].get();
+      dataMap["detail"] = snpapshop["detail"];
+      dataReport.add(dataMap);
+      data = dataReport;
+    });
+    data.sort((m1, m2) => m1["orderList"].compareTo(m2["orderList"]));
     return data;
   }
 
-  Future<List> reportDaily(String date)async{
-      int indexWhere = 0 ;
-      int count ;
-      menus.clear();
-      await getMenu();
-      data.forEach((order) {
-        if(date == order["orderDate"]){
+  Future<List> reportDaily(String date) async {
+    int indexWhere = 0;
+    int count;
+    menus.clear();
+    await getMenu();
+    data.forEach((order) {
+      if (date == order["orderDate"]) {
         int count_report = 0;
         num price_report = 0;
         // print(date);
-          order["detail"].forEach((orderDetail){
-            indexWhere = menus.indexWhere((food) => food["name"] == orderDetail["name"]);
-            if(indexWhere != -1){
-              count = orderDetail["count"];
-              count_report = menus[indexWhere]["count"] == null ? menus[indexWhere]["count"] = count : (count + menus[indexWhere]["count"]);
-              price_report = (count_report * menus[indexWhere]["price"]);
-              menus[indexWhere]["price_report"] = price_report;
-              menus[indexWhere]["count_report"] = count_report;
-            }
-          });
-        }
-      });
+        order["detail"].forEach((orderDetail) {
+          indexWhere =
+              menus.indexWhere((food) => food["name"] == orderDetail["name"]);
+          if (indexWhere != -1) {
+            count = orderDetail["count"];
+            count_report = menus[indexWhere]["count"] == null
+                ? menus[indexWhere]["count"] = count
+                : (count + menus[indexWhere]["count"]);
+            price_report = (count_report * menus[indexWhere]["price"]);
+            menus[indexWhere]["price_report"] = price_report;
+            menus[indexWhere]["count_report"] = count_report;
+          }
+        });
+      }
+    });
+    menus.sort((m2, m1) => m1["count_report"].compareTo(m2["count_report"]));
     return menus;
   }
 
+  int splitDateTimeYear(String text) {
+    var year, dateSplit;
+    dateSplit = text.split('/');
+    year = dateSplit[2];
+    return int.parse(year);
+  }
+
+  String splitDateMonth(String text) {
+    var my, dateSplit;
+    dateSplit = text.split('/');
+    my = "${dateSplit[1]}/${dateSplit[2]}";
+    return my;
+  }
+
+  String dateFormteMonth(int number) {
+    List moth = [
+      "ม.ค",
+      "ก.พ",
+      "มี.ค",
+      "เม.ย",
+      "พ.ค",
+      "มิ.ย",
+      "ก.ค",
+      "ส.ค",
+      "ก.ย",
+      "ต.ค",
+      "พ.ย",
+      "ธ.ค"
+    ];
+    return moth[(number - 1)];
+  }
+
+  List reportYear(String year) {
+    data_report.clear();
+    List price = [];
+    for (int i = 1; i <= 12; i++) {
+      price.add(0);
+      String my = "$i/$year";
+      data.forEach((orders) {
+        String orderYear = splitDateMonth(orders["orderDate"]);
+        if (orderYear == my) {
+          orders["detail"].forEach((ordersDetail) {
+            price[(i - 1)] += (ordersDetail["count"] * ordersDetail["price"]);
+          });
+        }
+      });
+      data_report.add(PopulationData(
+          moth: dateFormteMonth(i),
+          price: price[(i - 1)],
+          barColor:
+              charts.ColorUtil.fromDartColor(Color.fromRGBO(255, 51, 247, 1))));
+      print("${data_report[(i - 1)].moth} = ${data_report[(i - 1)].price}");
+    }
+
+    return data_report;
+  }
 }
 
 class PopulationData {
-  int year;
-  int population;
+  String moth;
+  int price;
   charts.Color barColor;
-  PopulationData({
-    @required this.year, 
-    @required this.population,
-    @required this.barColor
-  });
+  PopulationData(
+      {@required this.moth, @required this.price, @required this.barColor});
 }
--- lib/screens/report/report.dart
+++ lib/screens/report/report.dart
@@ -1,9 +1,12 @@
 import 'package:bsrufoods/controller/report_sale.dart';
+import 'package:bsrufoods/widget-picker/widget-cupertino-picker.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
 
 class Report extends StatefulWidget {
-  final String shopid ;
+  final String shopid;
   Report(this.shopid);
   @override
   _ReportState createState() => _ReportState();
@@ -11,6 +14,20 @@ class Report extends StatefulWidget {
 
 class _ReportState extends State<Report> {
   ReportSale report_sale = ReportSale();
+  List year = [
+    "2020",
+    "2021",
+    "2022",
+    "2023",
+    "2024",
+    "2025",
+    "2026",
+    "2027",
+    "2028",
+    "2029",
+    "2030"
+  ];
+  String dataReport;
 
   @override
   void initState() {
@@ -18,31 +35,81 @@ class _ReportState extends State<Report> {
     report_sale.getdata(widget.shopid);
   }
 
-  _getSeriesData(List data_report) {
+  _getSeriesData(String data_report) {
+    List _data = report_sale.reportYear(data_report);
+    setState(() {});
     List<charts.Series<PopulationData, String>> series = [
       charts.Series(
-        id: "Population",
-        data: data_report,
-        domainFn: (PopulationData series, _) => series.year.toString(),
-        measureFn: (PopulationData series, _) => series.population,
-        colorFn: (PopulationData series, _) => series.barColor
-      )
+          id: "Population",
+          data: _data,
+          domainFn: (PopulationData series, _) => series.moth,
+          measureFn: (PopulationData series, _) => series.price,
+          colorFn: (PopulationData series, _) => series.barColor)
     ];
     return series;
+  }
+
+  showdate() {
+    DatePicker.showDatePicker(context,
+        showTitleActions: true,
+        minTime: DateTime(2019, 1, 5),
+        maxTime: DateTime(2035, 6, 7),
+        onChanged: (date) {
+      dataReport = "${date.day}/${date.month}/${date.year}";
+      print(dataReport);
+    }, onConfirm: (date) {
+      dataReport = "${date.day}/${date.month}/${date.year}";
+    }, currentTime: DateTime.now(), locale: LocaleType.th);
+  }
+
+  Widget chartsReport() {
+    return Container(
+      height: 500,
+      child: charts.BarChart(_getSeriesData("2021"),
+          animate: true,
+          domainAxis: charts.OrdinalAxisSpec(
+              renderSpec: charts.SmallTickRendererSpec(labelRotation: 0),
+              showAxisLine: true)),
+    );
+  }
+
+  Widget picker() {
+    return Container(
+      height: 70,
+      width: 100,
+      child: CupertinoPicker(
+        children: List.generate(year.length, (index) {
+          return Text("${year[index]}");
+        }),
+        magnification: 2,
+        itemExtent: 25,
+        backgroundColor: Colors.white,
+        onSelectedItemChanged: (int index) {
+          print("${year[index]}");
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(),
-      body: Center(child: 
-        RaisedButton(
-          child: Text("ดูรายวัน"),
-          onPressed: (){
-            report_sale.reportDaily("24/4/2021");
-          },
-        )
-      ,),
-    );
+        appBar: AppBar(),
+        body: ListView(children: [
+          chartsReport(),
+          RaisedButton(
+            child: Text("ดูรายเดือน"),
+            onPressed: () {
+              showdate();
+              // print(dataReport);
+            },
+          ),
+          RaisedButton(
+            child: Text("ดูรายวัน"),
+            onPressed: () {
+              report_sale.reportDaily("24/4/2021");
+            },
+          )
+        ]));
   }
 }
